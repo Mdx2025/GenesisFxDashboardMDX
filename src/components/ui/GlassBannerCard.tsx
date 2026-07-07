e5b890c35bb48ea6d81948d76583428d81d61e00
Archer <archer@mdx.so>
2026-07-07 01:33:55 -0400

    fix: update GlassBannerCard padding to py-23 px-8, recalculate 3xl/4xl

--- src/components/ui/GlassBannerCard.tsx
+++ src/components/ui/GlassBannerCard.tsx
@@ -12,7 +12,7 @@ export function GlassBannerCard({ children, className = '', glowSrc = '/images/f
     <GlassCard variant="heavy" divider="green" rounded="26px" className={className}>
       <img src={glowSrc} alt="" aria-hidden="true" className="absolute top-0 left-0 w-[50%] pointer-events-none z-[1]" />
       <div className="absolute w-[493px] h-[278px] -left-[5%] -top-[80px] rounded-full [filter:url(#blur-157)] pointer-events-none bg-[#104030] opacity-50" aria-hidden="true" />
-      <div className="relative z-10 py-10 px-5 xl:py-14 xl:px-8 3xl:py-18 3xl:px-10 4xl:py-22 4xl:px-14">
+      <div className="relative z-10 py-10 px-5 xl:py-23 xl:px-8 3xl:py-29 3xl:px-10 4xl:py-36 4xl:px-14">
         {children}
       </div>
     </GlassCard>
