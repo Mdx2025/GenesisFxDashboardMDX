7e3072915c72ad768910159427177fde8b72737d
Archer <archer@mdx.so>
2026-07-06 22:48:24 -0400

    fix: move gradient ellipse to bottom-[-25%] to push glow below content

--- src/pages/WithdrawProcessingPage.tsx
+++ src/pages/WithdrawProcessingPage.tsx
@@ -151,7 +151,7 @@ export default function WithdrawProcessingPage() {
         </div>
       </div>
 
-      <div className="absolute left-1/2 -translate-x-1/2 bottom-0 w-[600px] h-[300px] rounded-full pointer-events-none bg-gfx-glow-green [filter:url(#blur-157)] will-change-transform" aria-hidden="true" />
+      <div className="absolute left-1/2 -translate-x-1/2 bottom-[-25%] w-[600px] h-[300px] rounded-full pointer-events-none bg-gfx-glow-green [filter:url(#blur-157)] will-change-transform" aria-hidden="true" />
 
       <div className="xl:hidden fixed bottom-6 left-1/2 -translate-x-1/2 z-50">
         <FloatingNavBar />
