e59e3dfa1da2ad2deca6f69d050ac06d77d328a9
Archer <archer@mdx.so>
2026-07-08 01:14:35 -0400

    fix: tooltip triggers only on info icon hover, remove unused HandCursorIcon

--- src/components/accounts/CreateAccountModal.tsx
+++ src/components/accounts/CreateAccountModal.tsx
@@ -34,14 +34,6 @@ function InfoIcon() {
   )
 }
 
-function HandCursorIcon() {
-  return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" aria-hidden="true">
-      <path d="M9.74976 1.5C8.51636 1.5 7.49976 2.5166 7.49976 3.75V12.6094L7.00757 12.0938L6.82007 11.9297C5.95581 11.0654 4.5437 11.0654 3.67944 11.9297C2.81519 12.7939 2.81519 14.2061 3.67944 15.0703V15.0938L9.82007 21.1641L9.86694 21.1875L9.89038 21.2344C10.9011 21.9932 12.1902 22.5 13.6404 22.5H14.9294C18.3308 22.5 21.0701 19.7607 21.0701 16.3594V10.5C21.0701 9.2666 20.0535 8.25 18.8201 8.25C18.5007 8.25 18.2048 8.33789 17.9294 8.46094C17.6833 7.48535 16.7957 6.75 15.7498 6.75C15.1755 6.75 14.6482 6.97559 14.2498 7.33594C13.8513 6.97559 13.324 6.75 12.7498 6.75C12.4861 6.75 12.2371 6.80566 11.9998 6.89062V3.75C11.9998 2.5166 10.9832 1.5 9.74976 1.5ZM9.74976 3C10.1658 3 10.4998 3.33398 10.4998 3.75V12H11.9998V9C11.9998 8.58398 12.3337 8.25 12.7498 8.25C13.1658 8.25 13.4998 8.58398 13.4998 9V12H14.9998V9C14.9998 8.58398 15.3337 8.25 15.7498 8.25C16.1658 8.25 16.4998 8.58398 16.4998 9V12H18.0701V10.5C18.0701 10.084 18.4041 9.75 18.8201 9.75C19.2361 9.75 19.5701 10.084 19.5701 10.5V16.3594C19.5701 18.958 17.5281 21 14.9294 21H13.6404C12.5417 21 11.5925 20.6074 10.8044 20.0156L4.73413 14.0156C4.40015 13.6816 4.40015 13.3184 4.73413 12.9844C5.06812 12.6504 5.4314 12.6504 5.76538 12.9844L8.99976 16.2188V3.75C8.99976 3.33398 9.33374 3 9.74976 3Z" fill="white"/>
-    </svg>
-  )
-}
-
 interface CreateAccountModalProps {
   open: boolean
   onClose: () => void
@@ -143,20 +135,20 @@ export function CreateAccountModal({ open, onClose }: CreateAccountModalProps) {
             Create Account
           </h2>
 
-          {/* Subtitle with info icon + tooltip */}
-          <div className="flex flex-col items-center mt-[12px] mb-[24px]">
+          {/* Subtitle with info icon + hover tooltip */}
+          <div className="flex justify-center mt-[12px] mb-[24px]">
             <div className="flex items-center gap-1.5">
-              <InfoIcon />
-              <span className="text-[#808080] text-[1rem] font-medium font-acid leading-[1.528rem]">Account Details</span>
-            </div>
-
-            {/* Tooltip popover */}
-            <div className="relative mt-[8px]">
-              <HandCursorIcon />
-              <div className="mt-1 bg-[#09241C] rounded-[14px] px-[2px] py-4 shadow-[0px_4px_8.9px_rgba(0,0,0,0.25)] text-center max-w-[221px]">
-                <p className="text-[#ECECEC] text-[1rem] font-medium font-acid leading-[1.528rem]">Account Details Email</p>
-                <p className="text-[#808080] text-[0.875rem] font-normal font-acid leading-[1.175rem] mt-2.5">Your TradeLocker account details will be emailed to you once your account is created.</p>
+              <div className="relative group">
+                <span className="cursor-pointer"><InfoIcon /></span>
+                {/* Tooltip on hover */}
+                <div className="absolute left-1/2 -translate-x-1/2 top-full mt-2 opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-opacity duration-200 z-50">
+                  <div className="bg-[#09241C] rounded-[14px] px-4 py-4 shadow-[0px_4px_8.9px_rgba(0,0,0,0.25)] text-center w-[221px]">
+                    <p className="text-[#ECECEC] text-[1rem] font-medium font-acid leading-[1.528rem]">Account Details Email</p>
+                    <p className="text-[#808080] text-[0.875rem] font-normal font-acid leading-[1.175rem] mt-2.5">Your TradeLocker account details will be emailed to you once your account is created.</p>
+                  </div>
+                </div>
               </div>
+              <span className="text-[#808080] text-[1rem] font-medium font-acid leading-[1.528rem]">Account Details</span>
             </div>
           </div>
 
